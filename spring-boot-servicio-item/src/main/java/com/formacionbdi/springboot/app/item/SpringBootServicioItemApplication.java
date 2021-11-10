0ede03ee697e86a9f89393827642461df273aec2
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2021-11-10 07:17:12 -0400

    Seccion 2-> Mapeando nuevo servicio Item con la api cliente HTTP RetTemplate

---
+++ spring-boot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringBootServicioItemApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServicioItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServicioItemApplication.class, args);
+	}
+
+}
---
+++ spring-boot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/Producto.java
@@ -0,0 +1,34 @@
+package com.formacionbdi.springboot.app.item.models;
+
+import java.util.Date;
+
+public class Producto {
+  private Long id;
+  private String nombre;
+  private Double precio;
+  private Date creteAt;
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public Double getPrecio() {
+	return precio;
+}
+public void setPrecio(Double precio) {
+	this.precio = precio;
+}
+public Date getCreteAt() {
+	return creteAt;
+}
+public void setCreteAt(Date creteAt) {
+	this.creteAt = creteAt;
+}
+}
